9621d6626dda814392543df9ec3d9c581e13bc92
herloncosta <herlon36@gmail.com>
2022-03-16 19:10:51 -0300

    implementando meuMap & tratando saida

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -1,16 +1,35 @@
 const service = require('./service')
 
+Array.prototype.meuMap = function(callback) {
+    const novoArrayMapeado = []
+    for (let i = 0; i < this.length - 1; i ++) {
+        const resultado = callback(this[i], i)
+        novoArrayMapeado.push(resultado)
+    }
+    return novoArrayMapeado
+}
+
 async function main() {
     try {
-        const nomes = []
         const result = await service.obterPessoa('a')
-        result.results.forEach(function(character) {
-            nomes.push({
-                name: character.name,
-                gender: character.gender
-            })
+        // const nomes = []
+        // result.results.forEach(function(pessoa) {
+        //     nomes.push({
+        //         nome: pessoa.name,
+        //         genero: pessoa.gender
+        //     })
+        // })
+
+        // const nomes = result.results.map(function(pessoa) {
+        //     return pessoa.name
+        // })
+
+        // refactoring
+        // const nomes = result.results.map(pessoa => pessoa.name)
+
+        const nomes = result.results.meuMap(function(pessoa, indice) {
+            return `[${indice}] ${pessoa.name}`
         })
-    
         console.log(nomes)
     } catch (error) {
         console.error('Falha na execução', error)
