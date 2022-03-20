b6cfc40cfe4ffa0c85a09b3a88cd72ce3b031c37
herloncosta <herlon36@gmail.com>
2022-03-20 18:51:30 -0300

    implementando cadastro

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,7 +1,8 @@
-const { readFile, readFileSync } = require('fs')
+const { readFile, writeFile } = require('fs')
 const { promisify } = require('util')
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 // outra forma de obter dados do json
 // const dadosJson = require('./herois.json')
@@ -12,12 +13,31 @@ class Database {
     }
 
     async obterDadosArquivo() {
-        const arquivo = await readFileSync(this.NOME_ARQUIVO, 'utf8')
+        const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
         return JSON.parse(arquivo.toString())
     }
 
-    escreverArquivo() {
+    async escreverArquivo(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
+    }
 
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id < 2 ? heroi.id : Date.now()
+
+        const heroiComId = {
+            id,
+            ...heroi
+        }
+
+        const dadosFinal = [
+            ...dados,
+            heroiComId
+        ]
+
+        const resultado = this.escreverArquivo(dadosFinal)
+        return resultado
     }
 
     async listar(id) {
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -5,14 +5,20 @@ const database = require('./database')
 const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'Velocidade', id: 1 }
 
 describe('Suite de manipulação de heróis', () => {
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
+
     it('deve pesquisar um herói usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
         const [ resultado ] = await database.listar(expected.id)
         deepEqual(resultado, expected)
     })
     
-    // it('deve cadastrar um herói, usando arquivos', async () => {
-    //     const expected = DEFAULT_ITEM_CADASTRAR
-    //     ok(null, expected)
-    // })
+    it('deve cadastrar um herói, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [ atual ] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(atual, expected)
+    })
 })
